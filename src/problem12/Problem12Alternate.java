b2d2d0cd279e2238294a5a4663aae954e6c0a486
pranavmathur <pranavmathur99@gmail.com>
2015-03-17 10:32:51 -0700

    Fixed Problem 12
    I wrote a new file and did not change the original.

--- src/problem12/Problem12Alternate.java
+++ src/problem12/Problem12Alternate.java
@@ -11,9 +11,41 @@ public class Problem12Alternate {
    }
    
    public static String reorder(String exp) {
+      if (exp.length() == 1)
+         return exp;
       String newExp = "(";
-      String operator = exp.substring(
-      return newExp;
+      String operator = exp.substring(1, 2);
+      String firstSubExp = "";
+      String secondSubExp = "";
+      if (exp.substring(3, 4).equals("(")) {
+         firstSubExp = exp.substring(3, findEndOfExpression(exp, 3) + 1);
+      }
+      else {
+         firstSubExp = exp.substring(3, 4);
+      }
+      if (exp.substring(firstSubExp.length() + 4, firstSubExp.length() + 5).equals("(")) {
+         secondSubExp = exp.substring(firstSubExp.length() + 4, findEndOfExpression(exp, firstSubExp.length() + 4) + 1);
+      }
+      else {
+         secondSubExp = exp.substring(firstSubExp.length() + 4, firstSubExp.length() + 5);
+      }   
+      return "(" + reorder(firstSubExp) + " " + operator + " " + reorder(secondSubExp) + ")";
    }
+   
+   public static int findEndOfExpression(String exp, int first) {
+      int stack = 1;
+      for (int i = first + 1; i < exp.length(); i++) {
+         if (exp.substring(i, i+1).equals("(")) {
+            stack++;  
+         }
+         else if (exp.substring(i, i+1).equals(")")) {
+            stack--;
+         }
+         if (stack == 0) {
+            return i;
+         }         
+      }    
+      return -1;    
+   }   
 
 }
